72d0afce3861365afa228907f3fc73a27e04e5f8
iAnd1710 <andrelima940@gmail.com>
2026-02-20 12:05:14 -0300

    chore: Update Google Tag Manager ID from GTM-KM7HQX8N to GTM-N35ZZJBP.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,17 +75,17 @@ export default defineNuxtConfig({
         {
           innerHTML: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KM7HQX8N');
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N35ZZJBP');
           `,
           type: 'text/javascript',
         },
       ],
       noscript: [
         {
-          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KM7HQX8N" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N35ZZJBP" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         },
       ],
     },
